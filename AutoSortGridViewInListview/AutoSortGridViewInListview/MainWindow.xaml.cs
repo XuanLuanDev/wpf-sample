6689a43b7747d272ee0cbb8b7936454afa6d50e2
Hyoko <hyoko662@gmail.com>
2022-03-05 00:21:18 +0700

    init

---
+++ AutoSortGridViewInListview/AutoSortGridViewInListview/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoSortGridViewInListview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Staff> staffs = new List<Staff>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            staffs.Add(new Staff()
+            {
+                 FirstName="Luan",
+                 LastName="Nguyen",
+                 DateOfBirth=new DateTime(1993,4,1)
+            });
+            staffs.Add(new Staff()
+            {
+                FirstName = "Nam",
+                LastName = "Nguyen",
+                DateOfBirth = new DateTime(1992, 4, 2)
+            });
+            staffs.Add(new Staff()
+            {
+                FirstName = "Huyen",
+                LastName = "Nguyen",
+                DateOfBirth = new DateTime(1990, 4, 2)
+            });
+            staffs.Add(new Staff()
+            {
+                FirstName = "Thai",
+                LastName = "Nguyen",
+                DateOfBirth = new DateTime(1992, 4, 3)
+            });
+            this.DataContext = staffs;
+        }
+    }
+}
---
+++ DataTemplateGridColumnByCondition/DataTemplateGridColumnByCondition/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataTemplateGridColumnByCondition
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Student> students = new List<Student>();
+            students.Add(new Student("SV1","Luan",9.5));
+            students.Add(new Student("SV2", "Nhan", 7.9));
+            students.Add(new Student("SV3", "Nam",8));
+            students.Add(new Student("SV4", "Ha",5.6));
+            students.Add(new Student("SV5", "Binh", 2.9));
+            this.DataContext = students;
+        }
+    }
+    public class Student
+    {
+        public Student(string code, string name, double totalScore)
+        {
+            Code = code; Name = name; TotalScore = totalScore;
+        }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double TotalScore { get; set; }
+    }
+    public class StudentTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate BadTemplate
+
+        { get; set; }
+        public DataTemplate MediumTemplate
+
+        { get; set; }
+        public DataTemplate GoodTemplate
+
+        { get; set; }
+        public DataTemplate ExcellentTemplate
+        {
+            get; set;
+        }
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+
+        {
+
+            Student student = item as Student;
+            if (student != null)
+
+            {
+
+                if (student.TotalScore >= 9)
+
+                    return ExcellentTemplate;
+
+                else if (student.TotalScore >= 8)
+
+                    return GoodTemplate;
+                else if (student.TotalScore >= 5)
+                    return MediumTemplate;
+                else return BadTemplate;
+            }
+
+            else
+
+                return base.SelectTemplate(item, container);
+
+        }
+    }
+}
